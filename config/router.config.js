fc1ad0c5e1ce6eb54a450a574361c9c0acdb2715
廖伟 <liaowei@liaoweideMacBook-Pro.local>
2018-09-07 14:49:32 +0800

    feat: 新增异常页面.

--- config/router.config.js
+++ config/router.config.js
@@ -29,6 +29,29 @@ export default [
         ],
       },
       {
+        name: 'exception',
+        path: '/exception',
+        hideChildrenInMenu: true,
+        routes: [
+          // exception
+          {
+            path: '/exception/403',
+            name: 'not-permission',
+            component: './Exception/403',
+          },
+          {
+            path: '/exception/404',
+            name: 'not-find',
+            component: './Exception/404',
+          },
+          {
+            path: '/exception/500',
+            name: 'server-error',
+            component: './Exception/500',
+          },
+        ],
+      },
+      {
         component: '404',
       },
     ],
