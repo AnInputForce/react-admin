a5032b17d78a04cf5bc7fb50664c480c1ef6747c
廖伟 <liaowei@liaoweideMacBook-Pro.local>
2018-09-09 19:47:53 +0800

    feat: 自定义用户布局.

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -25,7 +25,7 @@ const links = [
 
 const copyright = (
   <Fragment>
-    Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
+    Copyright <Icon type="copyright" /> 2018 React admin
   </Fragment>
 );
 
@@ -51,10 +51,10 @@ class UserLayout extends React.PureComponent {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>React Admin</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>React Admin 是基于Ant Design Pro 开发的示例工程</div>
           </div>
           {children}
         </div>
