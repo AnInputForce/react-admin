685b1dee1ecc0bb6c045c52203d54e61431b453c
廖伟 <24591106@qq.com>
2018-09-12 09:19:46 +0800

    feat: 修改页面Title为React Admin.

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -161,13 +161,13 @@ class BasicLayout extends React.PureComponent {
       }
     });
     if (!currRouterData) {
-      return 'Ant Design Pro';
+      return 'React Admin';
     }
     const message = formatMessage({
       id: currRouterData.locale || currRouterData.name,
       defaultMessage: currRouterData.name,
     });
-    return `${message} - Ant Design Pro`;
+    return `${message} - React Admin`;
   };
 
   getLayoutStyle = () => {
